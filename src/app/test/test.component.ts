9546fb41bad09cc2ab74b87ddd015d3c8a6e72e6
Juanara21 <110938196+Juanara21@users.noreply.github.com>
2023-05-21 11:39:36 -0500

    Mostrar preguntas

--- src/app/test/test.component.ts
+++ src/app/test/test.component.ts
@@ -1,4 +1,16 @@
 import { Component, OnInit } from '@angular/core';
+import { Data, Router } from '@angular/router';
+import { Question } from '../interfaces/question';
+import { QuestionService } from '../servicios/question.service';
+import {ModalDismissReasons, NgbModal} from '@ng-bootstrap/ng-bootstrap';
+import {AfterViewInit,  ViewChild} from '@angular/core';
+import {MatPaginator} from '@angular/material/paginator';
+import {MatTableDataSource} from '@angular/material/table';
+import { ToastrService } from 'ngx-toastr';
+import { HttpErrorResponse } from '@angular/common/http';
+import { ErrorService } from 'src/app/servicios/error_service';
+import { Career } from '../interfaces/career';
+import { CareerService } from '../servicios/career.service';
 
 @Component({
   selector: 'app-test',
@@ -7,9 +19,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TestComponent implements OnInit {
 
-  ngOnInit(): void {
-    
+  listQuestion: Question[] = [];
+
+  ngOnInit() {
+
+    this.obtenerCareer();
   }
-  
+
+  constructor(
+    private toastr: ToastrService, 
+    private router: Router, 
+    private _questionService: QuestionService,
+    private modalService: NgbModal,
+    private _errorService: ErrorService,
+    private _careerService: CareerService ) { }
+
+  obtenerCareer() {  
+
+    this._questionService.obtenerQuestion().subscribe((data: Question[]) => {
+    this.listQuestion = data   
+      
+    })
+  }
 
 }
